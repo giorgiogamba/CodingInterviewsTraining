0ef3391a36ae356f4b47ddf70534bdf7e4c500c4
Giorgio Gamba <giorgiogamba97@gmail.com>
2024-07-15 12:48:47 +0200

    add Permutations check method c++ implementation

--- main.cpp
+++ main.cpp
@@ -25,6 +25,38 @@ bool isUniqueChars(string s1)
     return true;
 }
 
+// Checks if the passed strings are a permutation of each other
+// Time/Space complexity O(n)
+bool arePermutations(string s1, string s2)
+{
+    // A permutation is a string with the same characters in different order.
+    // This means they have got the same length and the same character frequency
+
+    if (s1.length() != s2.length())
+        return false;
+
+    // We create a map that first of all counts the letters in the first string, then
+    // removes the frequencies throught the second string
+
+    vector<int> map(26, 0); // Lowercase english letters
+
+    // Adds frequencies
+    for (auto c : s1)
+        map[c - 'a'] ++; // The position is based on the letter value padded from the first alphabet letter
+
+    // Removes frequencies
+    for (auto c : s2)
+    {
+        map[c - 'a'] --;
+        // Because strings on the same length, when a letter is different, it must take some place < 0 
+        // also if the same character is not in the first string
+        if (map[c - 'a'] < 0) 
+            return false;
+    }
+
+    return true;
+}
+
 int main()
 {
     vector<string> stringDataset(6, "");
@@ -35,11 +67,24 @@ int main()
     stringDataset[4] = "agghhffdd";
     stringDataset[5] = "tyuyvdgnbn";
 
+    vector<string> stringDataset2(6, "");
+    stringDataset2[0] = "aaa";
+    stringDataset2[1] = "bca";
+    stringDataset2[2] = "baccb";
+    stringDataset2[3] = "defabc";
+    stringDataset2[4] = "ggffaddhh";
+    stringDataset2[5] = "gnbvdntyuyggg";
+
     std::cout << std::boolalpha;  
 
     for (auto s : stringDataset)
     {
         std::cout << "Input: " << s << " -- Result: " << isUniqueChars(s) << std::endl;
+    }
+
+    for (int i = 0; i < stringDataset.size(); i ++)
+    {
+        std::cout << "Input1: " << stringDataset[i] << " -- Input2: " << stringDataset2[i] << " -- Result: " << arePermutations(stringDataset[i], stringDataset2[i]) << std::endl;
     }
 
     return 0;
